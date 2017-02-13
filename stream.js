e734f9a8ba0b2551f83dd47b0932fd676003a681
Bowen Zhong <bz4e14@soton.ac.uk>
2017-02-13 16:55:46 +0800

    stream write

--- stream.js
+++ stream.js
@@ -1,36 +1,43 @@
-const http = require('http');
+const fs = require('fs');
+// const http = require('http');
 
-const server = http.createServer((req, res) => {
-	// http req and res are streams
-  // req is an http.IncomingMessage, which is a Readable Stream
-  // res is an http.ServerResponse, which is a Writable Stream
+// const server = http.createServer((req, res) => {
+// 	// http req and res are streams
+//   // req is an http.IncomingMessage, which is a Readable Stream
+//   // res is an http.ServerResponse, which is a Writable Stream
 
-  let body = '';
-  req.setEncoding('utf8');
+//   let body = '';
+//   req.setEncoding('utf8');
 
-  req.on('data', (chunk) => {
-  	body += chunk;
-  });
+//   req.on('data', (chunk) => {
+//   	body += chunk;
+//   });
 
-  req.on('end', () => {
-  	try {
-  		const data = JSON.parse(body);
+//   req.on('end', () => {
+//   	try {
+//   		const data = JSON.parse(body);
 
-  		// write back something
-  		res.write(typeof data);
-  		res.write(data);
-  		res.end();
-  	} catch (err) {
-  		res.statusCode = 400;
-  		return res.end(`error: ${err.message}`);
-  	}
-  });
+//   		// write back something
+//   		res.write(typeof data);
+//   		res.write(data);
+//   		res.end();
+//   	} catch (err) {
+//   		res.statusCode = 400;
+//   		return res.end(`error: ${err.message}`);
+//   	}
+//   });
+// });
+
+// server.listen(1337, () => {
+// 	console.log('server is listening on port 1337');
+// });
+
+
+const file = fs.createWriteStream('example.txt');
+file.write('hello, ');
+file.end('world!', () => {
+	console.log('stream write finish');
 });
-
-server.listen(1337, () => {
-	console.log('server is listening on port 1337');
-});
-
 
 
 
